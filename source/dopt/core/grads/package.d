1b5da48ceddd0bdfb23c4b63edabcb7387bfdb12
Henry Gouk <henry.gouk@gmail.com>
2017-07-06 01:46:17 +1200

    Implemented grad function. Currently assumes the objective function evaluates to a float32.

--- source/dopt/core/grads/package.d
+++ source/dopt/core/grads/package.d
@@ -16,7 +16,74 @@ static this()
 
 Operation[] grad(const(Operation) objective, const(Operation)[] wrt)
 {
-    assert(0, "Not implemented");
+    import std.algorithm : canFind, countUntil, map;
+    import std.array : array;
+    import std.conv : to;
+    import std.range : retro, zip;
+
+    enforce(objective.outputType.volume == 1, "The objective must have a volume of one");
+
+    const(Operation)[] ops;
+
+    void traverse(const(Operation) op)
+    {
+        foreach(d; op.deps)
+        {
+            if(!ops.canFind(d))
+            {
+                traverse(d);
+            }
+        }
+
+        ops ~= op;
+    }
+
+    //Topologically sort the operations
+    traverse(objective);
+
+    Operation[const(Operation)] grads;
+
+    //TODO: when I implement a 'ones' operation, replace this line
+    grads[objective] = float32([], [1.0f]);
+
+    //Iterate through the operations in reverse order (reverse mode autodiff)
+    foreach(op; ops.retro)
+    {
+        //Get the function that will let us compute the gradient of op w.r.t. its deps
+        auto gradFunc = mGradients.get(op.opType, null);
+        auto opGrad = grads[op];
+        
+        if(gradFunc is null || opGrad is null)
+        {
+            //This op, or its parent, is not differentiable, so we will just assume its derivative is zero everywhere
+            continue;
+        }
+
+        //Compute the derivative: d(op)/d(op.deps)
+        auto depGrads = gradFunc(op, opGrad);
+
+        //Add these to grads. If there is already an entry for one of the deps, then it has two parents.
+        //we can just add this grad to the existing grad, because maths.
+        foreach(d, g; zip(op.deps, depGrads))
+        {
+            auto currentGrad = grads.get(d, null);
+
+            if(currentGrad is null)
+            {
+                grads[d] = g;
+            }
+            else
+            {
+                grads[d] = currentGrad + g;
+            }
+        }
+    }
+
+    auto errIdx = wrt.countUntil!(x => grads.get(x, null) is null);
+
+    enforce(errIdx == -1, "Could not find wrt[" ~ errIdx.to!string ~ "] in the operation graph");
+
+    return wrt.map!(x => grads[x]).array();
 }
 
 void registerGradient(string opName, Gradient g)

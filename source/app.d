a9499cdb087da642d15397cba884489dedd5fa6a
Henry Gouk <henry.gouk@gmail.com>
2017-07-11 01:02:30 +1200

    app.d reports what operations are defined, implemented on CPU, and are differentiable

--- source/app.d
+++ source/app.d
@@ -3,12 +3,7 @@ void main()
     import dopt.core;
     import std.stdio;
 
-    auto a = float32([]);
-    auto b = float32([]);
-    auto c = float32([]);
-
-    auto d = a * b + c;
-
-    writeln("Expression: ", d);
-    writeln("Gradient: ", d.grad([a]));
+    writeln("Operations: ", listAllOperations());
+    writeln("CPU Operations: ", listAllCPUOperations());
+    writeln("Differentiable Operations: ", listAllGradients());
 }

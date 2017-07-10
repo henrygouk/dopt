febed9de412dd1a91b140876e2e7cbf15c6e1006
Henry Gouk <henry.gouk@gmail.com>
2017-07-11 01:01:02 +1200

    Added transpose gradient

--- source/dopt/core/grads/basic.d
+++ source/dopt/core/grads/basic.d
@@ -1,16 +1,34 @@
 module dopt.core.grads.basic;
 
+import dopt.core.grads;
 import dopt.core.ops;
 
 package
 {
     void initialize()
     {
+        import std.functional : toDelegate;
 
+        registerGradient("transpose", toDelegate(&transposeGrad));
     }
 }
 
 private
 {
-    
+    Operation[] transposeGrad(const(Operation) op, Operation parentGrad)
+    {
+        import std.algorithm : countUntil, map;
+        import std.array : array;
+        import std.range : iota;
+
+        auto order = op
+                    .attributes["order"]
+                    .get!(const(size_t)[]);
+
+        auto newOrder = iota(0, order.length)
+                       .map!(x => cast(size_t)order.countUntil(x))
+                       .array();
+
+        return [parentGrad.transpose(newOrder)];
+    }
 }

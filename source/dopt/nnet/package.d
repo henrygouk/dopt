07c4913eaed9163317c2c4fdad46c2b1843e1fb4
Henry Gouk <henry.gouk@gmail.com>
2017-10-25 22:50:07 +1300

    Added brief documentation to dopt.nnet

--- source/dopt/nnet/package.d
+++ source/dopt/nnet/package.d
@@ -1,3 +1,10 @@
+/**
+    This package contains a deep learning API backed by dopt.
+
+    A comprehensive example for how this package can be used is given in the $(D examples/mnist.d) file.
+
+    Authors: Henry Gouk
+*/
 module dopt.nnet;
 
 public

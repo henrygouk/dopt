0c83faeb361431fb4f1a022b1104673607fe1f7e
Henry Gouk <henry.gouk@gmail.com>
2018-01-11 14:23:36 +1300

    Fixed a few small problems with the VGG construction module.

--- source/dopt/nnet/models/vgg.d
+++ source/dopt/nnet/models/vgg.d
@@ -3,17 +3,17 @@ module dopt.nnet.models.vgg;
 import dopt;
 import dopt.nnet.models.maybe;
 
-Layer vgg16(Operation features, size_t[] denseLayerSizes = [4096, 4096, 1000], bool bn = true)
+Layer vgg16(Operation features, size_t[] denseLayerSizes = [4096, 4096], bool bn = false)
 {
-    auto sizes = [64, 64, -1, 128, 128, -1, 256, 256, 256, -1, 512, 512, 512, -1, 512, 512, 512];
+    auto sizes = [64, 64, -1, 128, 128, -1, 256, 256, 256, -1, 512, 512, 512, -1, 512, 512, 512, -1];
 
     return makeExtractor(features, sizes, 0.2f, bn)
           .makeTop(denseLayerSizes, 0.5f, bn);
 }
 
-Layer vgg19(Operation features, size_t[] denseLayerSizes = [4096, 4096, 1000], bool bn = true)
+Layer vgg19(Operation features, size_t[] denseLayerSizes = [4096, 4096], bool bn = false)
 {
-    auto sizes = [64, 64, -1, 128, 128, -1, 256, 256, 256, 256, -1, 512, 512, 512, 512, -1, 512, 512, 512, 512];
+    auto sizes = [64, 64, -1, 128, 128, -1, 256, 256, 256, 256, -1, 512, 512, 512, 512, -1, 512, 512, 512, 512, -1];
 
     return makeExtractor(features, sizes, 0.2f, bn)
           .makeTop(denseLayerSizes, 0.5f, bn);
@@ -36,10 +36,10 @@ private
             else
             {
                 layers = layers
+                        .maybeDropout(poolCtr == 0 ? 0.0f : drop)
                         .conv2D(s, [3, 3], new Conv2DOptions().padding([1, 1]))
                         .maybeBatchNorm(bn)
-                        .relu()
-                        .maybeDropout(poolCtr == 0 ? 0.0f : drop);
+                        .relu();
             }
         }
 
@@ -51,13 +51,9 @@ private
         foreach(i, s; sizes)
         {
             input = input
+                   .maybeDropout(drop)
                    .dense(s)
                    .relu();
-            
-            if(i != sizes.length - 1)
-            {
-                input = input.maybeDropout(drop);
-            }
         }
 
         return input;

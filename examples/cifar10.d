2a989201163caed437da523860c4aa252d9bffe6
Henry Gouk <henry.gouk@gmail.com>
2018-01-11 14:25:06 +1300

    Updated examples. Tweaked networks a little bit and added a lot of comments explaining what is happening.

--- examples/cifar10.d
+++ examples/cifar10.d
@@ -4,43 +4,93 @@ dependency "dopt" path=".."
 +/
 module cifar10;
 
+/*
+	This example trains a VGG19-style network on the CIFAR-10 dataset of tiny images.
+
+	VGG networks are fairly easy to understand, compared to some of the more recently presented models like GoogLeNet.
+	See ``Very Deep Convolutional Networks for Large-Scale Image Recognition'' by Simonyan and Zisserman for more
+	details. This example uses the dopt.nnet.models package to make defining a VGG model very easy.
+
+	The CIFAR-10 dataset contains 60,000 32x32 pixel colour images. Each of these images belongs to one of 10 classes.
+	In the standard setting, 50,000 of these images are used for training a model, and the other 10,000 are used for
+	evaluating how well the model works.
+*/
 void main(string[] args)
 {
 	import std.algorithm : joiner, maxIndex;
 	import std.array : array;
 	import std.range : zip, chunks;
-	import std.stdio : stdout, write, writeln;
+	import std.stdio : stderr, stdout, write, writeln;
 
 	import dopt.core;
 	import dopt.nnet;
 	import dopt.online;
 
+	if(args.length != 2)
+	{
+		stderr.writeln("Usage: cifar10.d <data directory>");
+		return;
+	}
+
+	//Loads the CIFAR-10 dataset. Download this in the binary format from https://www.cs.toronto.edu/~kriz/cifar.html
+	writeln("Loading data...");
     auto data = loadCIFAR10(args[1]);
 
-    auto features = float32([100, 3, 32, 32]);
-    auto labels = float32([100, 10]);
+	/*
+	Now we create two variable nodes. ``features'' is used to represent a minibatch of input images, and ``labels''
+	will be used to represent the label corresponding to each of those images.
+	*/
+	writeln("Constructing network graph...");
+	size_t batchSize = 100;
+    auto features = float32([batchSize, 3, 32, 32]);
+    auto labels = float32([batchSize, 10]);
 
-    auto preds = vgg19(features, [512, 512, 10], false).softmax();
+	/*
+	There are a few predefined models in dopt.nnet.models, such as vgg19. We provide it with the variable we want to
+	use as the input to this model, tell it what sizes the fully connected layers should be, and then put a softmax
+	activation function on the end. The softmax function is the standard activation function when one is performing
+	a classification task.
+	*/
+    auto preds = vgg19(features, [512, 512])
+				.dense(10)
+				.softmax();
     
+	//The DAGNetwork class takes the inputs and outputs of a network and aggregates parameters in several different.
     auto network = new DAGNetwork([features], [preds]);
 
+	/*
+	Layer objects have both ``output'' and ``trainOutput'' fields, because operations like dropout perform different
+	computations at train and test time. Therefore, we construct two different loss symbols: one for optimising, and
+	one for evaluating.
+	*/
     auto lossSym = crossEntropy(preds.trainOutput, labels) + network.paramLoss;
 	auto testLoss = crossEntropy(preds.output, labels);
 
+	/*
+	Now we set up an optimiser. Adam is good for proof of concepts, due to the fast convergence, however the
+	performance of the final model is often slightly worse than that of a model trained with SGD+momentum.
+	*/
+	writeln("Creating optimiser...");
 	auto learningRate = float32([], [0.0001f]);
 	auto updater = adam([lossSym], network.params, network.paramProj, learningRate);
 
+	writeln("Training...");
+
+	//Iterate for 120 epochs of training!
 	foreach(e; 0 .. 120)
 	{
 		float tot = 0;
 
+		//Decreasing the learning rate after a while often results in better performance.
 		if(e == 100)
 		{
 			learningRate.value.as!float[0] = 0.00001f;
 		}
 
-		foreach(fs, ls; zip(data.trainFeatures.chunks(100), data.trainLabels.chunks(100)))
+		//Iterate through all the training data, creating minibatches as we go.
+		foreach(fs, ls; zip(data.trainFeatures.chunks(batchSize), data.trainLabels.chunks(batchSize)))
 		{
+			//Make an update to the model parameters using the minibatch of training data
 			updater([
 				features: Buffer(fs.joiner().array()),
 				labels: Buffer(ls.joiner().array())
@@ -48,6 +98,7 @@ void main(string[] args)
 
 			tot++;
 
+			//Let the user know that we are making some progress
             write("  ", tot, "/500    \r");
             stdout.flush();
 		}
@@ -55,7 +106,11 @@ void main(string[] args)
 		int correct;
 		int total;
 
-		foreach(fs, ls; zip(data.testFeatures.chunks(100), data.testLabels.chunks(100)))
+		/*
+		After an epoch of training, show how well the model is performing on the test set---this could be done less
+		frequently, or on a validation set if one is still tuning the structure/hyperparams of the model.
+		*/
+		foreach(fs, ls; zip(data.testFeatures.chunks(batchSize), data.testLabels.chunks(batchSize)))
 		{
 			auto pred = network.outputs[0].evaluate([
 				features: Buffer(fs.joiner().array())
@@ -72,6 +127,7 @@ void main(string[] args)
 			}
 		}
 
+		//Tell the user how accurate the model is on the test data
 		writeln(e + 1, ": ", correct / cast(float)total);
 	}
 }
--- examples/mnist.d
+++ examples/mnist.d
@@ -4,22 +4,39 @@ dependency "dopt" path=".."
 +/
 module mnist;
 
+/*
+	This example trains a small convolutional network on the MNIST dataset of hand-written images. The network used is
+	very small by today's standards, but MNIST is a very easy dataset so this does not really matter.
+
+	The MNIST dataset contains small monochrome images of hand-written digits, and the goal is to determine which digit
+	each image contains.
+*/
 void main(string[] args)
 {
 	import std.algorithm : joiner, maxIndex;
 	import std.array : array;
 	import std.range : zip, chunks;
-	import std.stdio : writeln;
+	import std.stdio : stderr, writeln;
 	
 	import dopt.core;
 	import dopt.nnet;
 	import dopt.online;
 
+	if(args.length != 2)
+	{
+		stderr.writeln("Usage: mnist.d <data directory>");
+		return;
+	}
+
+	//Load the minst dataset of hand-written digits. Download the binary files from http://yann.lecun.com/exdb/mnist/
     auto data = loadMNIST(args[1]);
 
-    auto features = float32([100, 1, 28, 28]);
-	auto labels = float32([100, 10]);
+	//Create the variables nodes required to pass data into the operation graph
+	size_t batchSize = 100;
+    auto features = float32([batchSize, 1, 28, 28]);
+	auto labels = float32([batchSize, 10]);
 
+	//Construct a small convolutional network
 	auto preds = dataSource(features)
 				.conv2D(32, [5, 5])
 				.relu()
@@ -30,18 +47,29 @@ void main(string[] args)
 				.dense(10)
 				.softmax();
 
+	//Construct the DAGNetwork object that can be used to collate all the parameters and loss terms
 	auto network = new DAGNetwork([features], [preds]);
 
+	//Create a symbol to represent the training loss function
 	auto lossSym = crossEntropy(preds.trainOutput, labels) + network.paramLoss;
 
-	auto updater = adam([lossSym], network.params, null);
+	//Create an optimiser that can use minibatches of labelled data to update the weights of the network
+	auto lr = float32([], [0.001f]);
+	auto updater = adam([lossSym], network.params, network.paramProj);
 
-	foreach(e; 0 .. 10)
+	//Iterate for 10 epochs of training
+	foreach(e; 0 .. 40)
 	{
 		float totloss = 0;
 		float tot = 0;
 
-		foreach(fs, ls; zip(data.trainFeatures.chunks(100), data.trainLabels.chunks(100)))
+		if(e == 30)
+		{
+			lr.value.as!float[0] = 0.0001f;
+		}
+
+		//Iterate over each minibatch of data and perform an update of the model parameters
+		foreach(fs, ls; zip(data.trainFeatures.chunks(batchSize), data.trainLabels.chunks(batchSize)))
 		{
 			auto loss = updater([
 				features: Buffer(fs.joiner().array()),
@@ -52,6 +80,7 @@ void main(string[] args)
 			tot++;
 		}
 
+		//Write out the training loss for this epoch
 		writeln(e, ": ", totloss / tot);
 	}
 
@@ -60,12 +89,15 @@ void main(string[] args)
 
 	import std.stdio : writeln;
 
-	foreach(fs, ls; zip(data.testFeatures.chunks(100), data.testLabels.chunks(100)))
+	//Iterate over each minibatch in the test set
+	foreach(fs, ls; zip(data.testFeatures.chunks(batchSize), data.testLabels.chunks(batchSize)))
 	{
+		//Make some predictions for this minibatch
 		auto pred = network.outputs[0].evaluate([
 			features: Buffer(fs.joiner().array())
 		]).as!float;
 
+		//Determine the accuracy of these predictions using the ground truth data
 		foreach(p, t; zip(pred.chunks(10), ls))
 		{
 			if(p.maxIndex == t.maxIndex)
@@ -77,5 +109,6 @@ void main(string[] args)
 		}
 	}
 
+	//Write out the accuracy of the model on the test set
 	writeln(correct / cast(float)total);
 }
